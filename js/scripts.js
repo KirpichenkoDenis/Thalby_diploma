ff0b8c992aac2eb7ae3f3b5cabf5f3111db6dedc
denis <kirpichenkodenis01@gmail.com>
2023-02-03 13:00:37 +0200

    03.02

---
+++ js/scripts.js
@@ -0,0 +1,66 @@
+$(document).ready(function(){
+
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:0,
+        nav:true,
+        navText: [ '', ' ' ],
+
+        responsive:{
+                0:{
+                    items:1
+            },
+
+            400:{
+                items:2
+            },
+
+            1000:{
+                items:3
+            }
+        }
+    });
+    $( function() {
+        $( "#accordion" ).accordion();
+    } );
+});
+
+(function ($) {
+    "use strict";
+
+    //Navigation
+    var app = function () {
+        var body = undefined;
+        var menu = undefined;
+        var menuItems = undefined;
+        var init = function init() {
+            body = document.querySelector('body');
+            menu = document.querySelector('.menu-icon');
+            menuItems = document.querySelectorAll('.nav__list-item');
+            applyListeners();
+        };
+        var applyListeners = function applyListeners() {
+            menu.addEventListener('click', function () {
+                return toggleClass(body, 'nav-active');
+            });
+        };
+        var toggleClass = function toggleClass(element, stringClass) {
+            if (element.classList.contains(stringClass)) element.classList.remove(stringClass); else element.classList.add(stringClass);
+        };
+        init();
+    }();
+
+
+    //Switch light/dark
+
+    $("#switch").on('click', function () {
+        if ($("body").hasClass("light")) {
+            $("body").removeClass("light");
+            $("#switch").removeClass("switched");
+        } else {
+            $("body").addClass("light");
+            $("#switch").addClass("switched");
+        }
+    });
+
+})(jQuery);
